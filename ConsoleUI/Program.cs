f486d12a86e6c15b04d068f5045d4622a1108843
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 15:11:02 -0500

    Create Code
    Create a Console Application with a for loop that throws an exception after five iterations. Catch the exception.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			int[] numbers = new int[] { 1, 42, 34, 56, 13 };
+
+			for ( int i = 0; i <= numbers.Length; i++ )
+			{
+				try
+				{
+					Console.WriteLine($"The number at position {i} is {numbers[i]}");
+				}
+				catch ( Exception ex )
+				{
+					Console.WriteLine("We had an error");
+					Console.WriteLine(ex);
+				}
+			}
+
+			_ = Console.ReadLine();
 		}
 	}
 }
